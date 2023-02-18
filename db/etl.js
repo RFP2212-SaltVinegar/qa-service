e0beeb35cf887d650edc2a1adf60985e7df68d38
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-02-18 14:17:21 -0800

    working in new branch -updating branch

--- db/etl.js
+++ db/etl.js
@@ -8,58 +8,58 @@ const transformBody = (body) => {
   return trimmedBody;
 }
 
-csvToJSON().fromFile('db/questions.csv')
-  .then((testFile) => {
-    fs.writeFileSync('db/questionsTransformed.csv', jsonToCsv(testFile.map(row => {
-      return {
-        id:row.id,
-        product_id: row.question_id,
-        body: transformBody(row.body),
-        date_added: row.date_written,
-        asker: row.asker_name,
-        reported: row.reported,
-        helpful: row.helpful
-      }
-    }), { fields: ['id', 'product_id', 'body', 'date_added', 'asker', 'reported', 'helpful']}));
-  } )
+// csvToJSON().fromFile('db/data/questions.csv')
+//   .then((testFile) => {
+//     fs.writeFileSync('db/data/questionsTransformed.csv', jsonToCsv(testFile.map(row => {
+//       return {
+//         id:row.id,
+//         product_id: row.question_id,
+//         body: transformBody(row.body),
+//         date_added: row.date_written,
+//         asker: row.asker_name,
+//         reported: row.reported,
+//         helpful: row.helpful
+//       }
+//     }), { fields: ['id', 'product_id', 'body', 'date_added', 'asker', 'reported', 'helpful']}));
+//   } )
 
-csvToJSON().fromFile('db/answers.csv')
-  .then((testFile) => {
-    fs.writeFileSync('db/answersTransformed.csv', jsonToCsv(testFile.map(row => {
-      return {
-        id:row.id,
-        question_id: row.question_id,
-        body: transformBody(row.body),
-        date_added: row.date_written,
-        answerer: row.answerer_name,
-        reported: row.reported,
-        helpful: row.helpful
-      }
-    }), { fields: ['id', 'question_id', 'body', 'date_added', 'answerer', 'reported', 'helpful']}));
-  } )
+// csvToJSON().fromFile('db/data/answers.csv')
+//   .then((testFile) => {
+//     fs.writeFileSync('db/data/answersTransformed.csv', jsonToCsv(testFile.map(row => {
+//       return {
+//         id:row.id,
+//         question_id: row.question_id,
+//         body: transformBody(row.body),
+//         date_added: row.date_written,
+//         answerer: row.answerer_name,
+//         reported: row.reported,
+//         helpful: row.helpful
+//       }
+//     }), { fields: ['id', 'question_id', 'body', 'date_added', 'answerer', 'reported', 'helpful']}));
+//   } )
 
-  csvToJSON().fromFile('db/answers_photos.csv')
+  csvToJSON().fromFile('db/data/answers_photos.csv')
   .then((testFile) => {
-    fs.writeFileSync('db/answersPhotosTransformed.csv', jsonToCsv(testFile.map(row => {
+    fs.writeFileSync('db/data/answersPhotosTransformed.csv', jsonToCsv(testFile.map(row => {
       return {
         id:row.id,
         answer_id: row.answer_id,
-        url: row.url
+        url: transformBody(row.url)
       }
     }), { fields: ['id', 'answer_id', 'url']}));
   } )
 
-  csvToJSON().fromFile('db/testETL.csv')
-  .then((testFile) => {
-    fs.writeFileSync('db/testETLTransformed.csv', jsonToCsv(testFile.map(row => {
-      return {
-        id:row.id,
-        question_id: row.question_id,
-        body: transformBody(row.body),
-        date_added: row.date_written,
-        answerer: row.answerer_name,
-        reported: row.reported,
-        helpful: row.helpful
-      }
-    }), { fields: ['id', 'question_id', 'body', 'date_added', 'answerer', 'reported', 'helpful']}));
-  } )
+  // csvToJSON().fromFile('db/testETL.csv')
+  // .then((testFile) => {
+  //   fs.writeFileSync('db/testETLTransformed.csv', jsonToCsv(testFile.map(row => {
+  //     return {
+  //       id:row.id,
+  //       question_id: row.question_id,
+  //       body: transformBody(row.body),
+  //       date_added: row.date_written,
+  //       answerer: row.answerer_name,
+  //       reported: row.reported,
+  //       helpful: row.helpful
+  //     }
+  //   }), { fields: ['id', 'question_id', 'body', 'date_added', 'answerer', 'reported', 'helpful']}));
+  // } )
