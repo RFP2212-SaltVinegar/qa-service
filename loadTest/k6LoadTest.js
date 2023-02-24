533b998939fabeaf051551dfd8eb62403a4f8778
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-02-23 20:30:19 -0800

    DB Tables indexed, stress test optimized

--- loadTest/k6LoadTest.js
+++ loadTest/k6LoadTest.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 
-require('dotenv').config();
+const URL = `http://localhost:3000`;
 
 export function handleSummary(data) {
   return {
@@ -12,20 +12,27 @@ export function handleSummary(data) {
 
 export let options = {
   stages: [
-    { duration: '1m', target: 100 },
-    { duration: '3m', target: 100 },
-    { duration: '1m', target: 300 },
-    { duration: '3m', target: 300 }
-  ]
+    { duration: '30s', target: 100 },
+    { duration: '2m', target: 100 },
+    { duration: '30s', target: 300 },
+    { duration: '2m', target: 300 }
+  ],
+  thresholds: {
+    // During the whole test execution, the error rate must be lower than 1%.
+    http_req_failed: ['rate<0.01'],
+    // 90% of requests must finish within 400ms.
+    http_req_duration: ['p(90) < 20000']
+  }
 };
 
 export default () => {
+  let pId = Math.floor(Math.random() * 1000000);
   let qId = Math.floor(Math.random() * 3518950);
   let aId = Math.floor(Math.random() * 6879290);
-  let pId = Math.floor(Math.random() * 1000000);
   http.batch([
-    ['PUT', `http://${process.env.SITE_URL}:${process.env.PORT}/qa/questions/${qId}/helpful`],
-    ['PUT', `http://${process.env.SITE_URL}:${process.env.PORT}/qa/answers/${aId}/helpful`]
+    ['GET', `${URL}/qa/questions?product_id=${pId}`],
+    ['PUT', `${URL}/qa/questions/${qId}/helpful`],
+    ['PUT', `${URL}/qa/answers/${aId}/helpful`]
   ]);
   sleep(1);
 };
